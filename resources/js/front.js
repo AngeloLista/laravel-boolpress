5e45f33e1c0a911a6dc62dbcfa8dc5db36249f9a
AngeloLista <abstractplayer@gmail.com>
2022-04-05 17:11:51 +0200

    imported router in front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -8,9 +8,11 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import router from './routes'
 import App from './components/App.vue';
 
 const root = new Vue({
     el: '#root',
+    router,
     render: h => h(App)
 });
