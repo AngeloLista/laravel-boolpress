68c19fce88e32c37344bde24d32972b933bf3bbf
AngeloLista <abstractplayer@gmail.com>
2022-04-05 17:12:05 +0200

    created HomePage component

---
+++ resources/js/components/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <PostList />
+  </div>
+</template>
+
+<script>
+import PostsList from "../posts/PostsList.vue";
+export default {
+  name: "HomePage",
+  component: {
+    PostsList,
+  },
+};
+</script>
+
+<style>
+</style>
