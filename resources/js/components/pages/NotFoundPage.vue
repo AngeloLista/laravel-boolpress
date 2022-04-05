95ff2606aef23c91d99a98ef7e79d8c19c967e27
AngeloLista <abstractplayer@gmail.com>
2022-04-05 17:39:24 +0200

    created NotFoundPage component

---
+++ resources/js/components/pages/NotFoundPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <section
+    id="not-found"
+    class="d-flex justify-content-center align-items-center"
+  >
+    <h3 class="h1">404 | Not Found</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage",
+};
+</script>
+
+<style lang="scss" scoped>
+#not-found {
+  height: calc(100vh - 55px);
+}
+</style>
