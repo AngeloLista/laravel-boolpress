20b1c6b8c89708fe9e78a3ff71083aae942e15eb
AngeloLista <abstractplayer@gmail.com>
2022-04-08 01:22:03 +0200

    added ContactMail API validation

--- app/Http/Controllers/Api/ContactMessageController.php
+++ app/Http/Controllers/Api/ContactMessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 use App\Mail\ContactMessageMail;
 
 class ContactMessageController extends Controller
@@ -12,6 +13,15 @@ class ContactMessageController extends Controller
     public function send(Request $request)
     {
         $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors', $validator->errors()]);
+        }
 
         $mail = new ContactMessageMail($data);
         Mail::to(env('MAIL_ADMIN_ADDRESS'))->send($mail);
